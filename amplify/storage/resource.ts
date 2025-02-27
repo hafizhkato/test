16f53d1b1dc3d6d1764525bab4d5ce80dfed3647
Hafizh Zulkifli <hafizhzulkifli@gmail.com>
2025-02-27 13:51:30 +0800

    change s3 permission

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -4,12 +4,8 @@ import { defineStorage } from '@aws-amplify/backend';
 export const storage = defineStorage({
   name: 'UserStorages',
   access: (allow) => ({
-    'private-images/{entity_id}/*': [
-      allow.guest.to(['read']),
-      allow.entity('identity').to(['read', 'write', 'delete'])
-    ],
     'image-documents/{entity_id}/*': [
-      allow.authenticated.to(['read','write', 'delete']),
+      allow.entity('identity').to(['read', 'write', 'delete'])
     ],
 
 }),
